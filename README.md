8b01c8eded89a8e4ae699e0a783128cc57b9bea4
Mike Prince <mike@mikeprince.com>
2025-03-23 23:07:44 -0700

    Added function to test pushing profile to production services

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ With this service running locally:
 
 	$ node test/create-local-agentic-profile
 
+## Configuring AWS
+
+1. Create a Lambda function
+	- Node 22.x
+	- ARM CPU
+2. Configure a new HTTP API Gateway 
+	- When listing APIs, click "Create API" button
+	- Under HTTP API, click "Build"
+	- API name: agentic-profile-test-api
+	- Integrations: Lambda
+	- Lambda function: ...test-service
+	- Version: 1.0
+	- "Create"
+3. Create a Custom Domain Name
+	- Ensure an SSL certificate for the domain name
+	- Domain name: test-api.agenticprofile.ai
+4. Use Route 53 to map the endpoint to the API gateway
+5. Make sure to add AWS lambda permissions
+6. Make sure to add Custom domain API mapping
---
+++ test/create-public-agentic-profile.js
@@ -0,0 +1,86 @@
+import axios from "axios";
+
+import {
+    createEdDsaJwk
+} from "@agentic-profile/auth";
+import { webDidToUrl } from "@agentic-profile/common";
+
+/*
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { writeFile, mkdir } from "fs/promises";
+*/
+
+import { logAxiosResult } from "./util.js";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+async function generateKeys( name ) {
+    const keypair = await createEdDsaJwk();
+    const shared = {
+        name,
+        ...keypair,
+        privateKey: undefined,
+        created: new Date(),
+        expires: new Date('2030-1-1')
+    };
+    return { keypair, shared };
+}
+
+(async ()=>{
+
+    const { publicJwk, b64uPublicKey, privateJwk } = await createEdDsaJwk();
+    const verificationMethod = {
+        id: "#agent-key-0",
+        type: "JsonWebKey2020",
+        publicKeyJwk: publicJwk
+    };
+
+    const profile = {
+        "@context": [
+            "https://www.w3.org/ns/did/v1",
+            "https://w3id.org/security/suites/jws-2020/v1",
+            "https://iamagentic.org/ns/agentic-profile/v1"
+        ],
+        //id: did,
+        name: "Dave",
+        verificationMethod: [],
+        service:[
+            {
+                id: "#agentic-chat",
+                type: "AgenticChat",
+                serviceEndpoint: "https://localhost:3003/users/7/agent-chats",
+                capabilityInvocation: [
+                    verificationMethod
+                ]
+            }
+        ]
+    };
+
+    try {
+        const result = await axios.post(
+            "https://test-api.agenticprofile.ai/agentic-profile",
+            { profile, b64uPublicKey }
+        );
+        logAxiosResult( result );
+        const data = result.data ?? result.response?.data;
+
+        const savedProfile = data.profile;
+        const profileJSON = JSON.stringify( savedProfile, null, 4 );
+        const did = savedProfile.id;
+
+        const keyringJSON = JSON.stringify( privateJwk, null, 4 );
+        console.log(`Agentic Profile saved
+
+Shhhh! Keyring for testing: ${keyringJSON}
+
+Agentic Profile: ${profileJSON}
+
+DID: ${did}
+HTTPS URL: ${webDidToUrl(did)} 
+`);
+    } catch (error) {
+        console.error("ERROR: Failed to create profile:", error);
+    }
+})();
